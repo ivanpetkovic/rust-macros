291870941b35b1fd3dd42f13adac44718c83b712
ivanp <ivan@nignite.com>
2022-03-21 08:21:44 +0100

    initial

--- test/require_feature.rs
+++ test/require_feature.rs
@@ -3,9 +3,14 @@ use crate::require_feature;
 
 
 #[require_feature]
-struct El {}
+struct Student {
+    name: String
+}
 
 #[test]
 fn it_works() {
-    let result = Els;
+    let result = Student {
+        name: String::from("Ivan")
+    };
+    assert_eq!(result.n)
 }
